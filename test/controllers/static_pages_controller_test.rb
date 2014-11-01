7e94b64c203501b2d6b0f9e15f9bd1ae0bdf84fe
Max Wofford <max.speed.wofford@gmail.com>
2014-11-01 13:33:29 -0600

    assert static page successful response

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,11 +3,13 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
+    assert_response :success
     assert_select "title", "Home | Collaborart"
   end
 
   test "should get about" do
     get :about
+    assert_response :success
     assert_select "title", "About | Collaborart"
   end
 
