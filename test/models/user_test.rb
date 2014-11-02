8ccd8ba83ec55f701938b3076ad29a377fe761ae
Max Wofford <max.speed.wofford@gmail.com>
2014-11-01 22:42:44 -0600

    test user/project/artwork_iteration creation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create users with projects with art iterations" do
+    (0..100).each do |i|
+      @user = User.create!(:email => "test#{i}@example.com", :password => 'password', :password_confirmation => 'password')
+      ArtworkIteration.create!(:user_id => @user.id, :name => "Artwork #{i}")
+    end
+    @last_id = User.last.id
+    assert_equal(@last_id, User.last.artwork_iterations.last.user_id)
+    assert_equal(@last_id, User.last.projects.last.user_id)
+  end
 end
